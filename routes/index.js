5af73740682b5f68720521bfe78d4e84578f5d99
Rodrigo Galvez <rodrigogalvezdiaz@outlook.com>
2015-07-12 00:35:55 +0200

    Rama créditos con autor

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,14 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-//  res.render('index', { title: 'Express' });
   res.render('index', { title: 'Quiz' });
 });
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+// GET /author
+router.get('/author', function(req, res) {
+  res.render('author');
+});
 
 module.exports = router;
